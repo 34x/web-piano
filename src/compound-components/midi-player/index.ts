211bd15253bc961fcf94d8ebf6b8df88b86a5517
34x <maks@34x.me>
2020-07-16 17:45:22 +0200

    Transfer all code from midi.ts to appropriate components, update main app to make use of the new midi player object

--- src/compound-components/midi-player/index.ts
+++ src/compound-components/midi-player/index.ts
@@ -1,17 +1,64 @@
-class MidiPlayer {
-	play() {
+import { MidiReader, MidiReaderEvent } from 'src/components/midi-reader';
+import Soundfont from 'soundfont-player';
 
-	}
+export enum MidiPlayerState {
+    playing,
+    paused,
+    idle
+}
 
-	pause() {
+export class MidiPlayer {
+    private state: MidiPlayerState = MidiPlayerState.idle;
+    private reader: MidiReader;
+    private instrument: any;
+    private onStateChangeHandler: (event: any) => void;
 
-	}
+    constructor() {
+        this.reader = new MidiReader();
+        this.reader.on(MidiReaderEvent.noteOn, this.onNoteOn.bind(this));
+    }
 
-	stop() {
+    getState(): MidiPlayerState {
+        return this.state;
+    }
 
-	}
+    play() {
+        this.changeState(MidiPlayerState.playing);
+        this.reader.startReading();
+    }
 
-	setInstrument(instrument: any, track: number) {
+    pause() {
+        this.changeState(MidiPlayerState.paused);
+        this.reader.stopReading();
+    }
 
-	}
+    stop() {
+        this.changeState(MidiPlayerState.idle);
+        this.reader.stopReading();
+    }
+
+    async loadUrl(url: string) {
+        await this.reader.loadUrl(url);
+        const audioContext = new AudioContext();
+        this.instrument = await Soundfont.instrument(audioContext, 'acoustic_grand_piano');
+    }
+
+    // setInstrument(instrument: any, track: number) {
+
+    // }
+
+    onStateChange(handler: (event: any) => void) {
+        this.onStateChangeHandler = handler;
+    }
+
+    private changeState(state: MidiPlayerState) {
+        this.state = state;
+        if (this.onStateChangeHandler) {
+            this.onStateChangeHandler({ state: state });
+        }
+    }
+
+    private onNoteOn(event: any) {
+        this.instrument.play(event.noteName)
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
-import { hello } from './hello';
-import { hello as hello2 } from './hello2';
-import { MidiPlayer } from './midi';
 import App from "./app.svelte";
 
 export function main() {
-    var app = new App({
+    const app = new App({
         target: document.body,
-      });
-    // hello('Hello 1');
-    // hello2('Hello 2');
-    // const player = new MidiPlayer();
-    // player.play();
+    });
 }
