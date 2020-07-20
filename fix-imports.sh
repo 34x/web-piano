4ad7e16caaacad978df1c55d1681c3f6f26174c4
34x <maks@34x.me>
2020-07-20 15:43:56 +0200

    Fix mid folder imports after build

--- fix-imports.sh
+++ fix-imports.sh
@@ -3,3 +3,4 @@
 # we are using hithub pages wit web-piano prefix for now
 # can be removed after moving to a custom domain or snowpack fix
 find build/bundle -type f -name "*.js" -print0 | xargs -0 sed -i'.js.bkp' 's|"/bundle/|"/web-piano/bundle/|g'
+find build/bundle -type f -name "*.js" -print0 | xargs -0 sed -i'.js.bkp' 's|"/midi/|"/web-piano/midi/|g'
