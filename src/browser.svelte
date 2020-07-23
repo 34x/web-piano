5ed602d2ced8a2fd08993522dbe73e9761d1e0ba
Artem <artnerab@gmail.com>
2020-07-23 13:32:17 +0300

    Check the browser and show screen with download links if the browser doesn't fit

---
+++ src/browser.svelte
@@ -0,0 +1,34 @@
+<script>
+    const browsers = [
+        {name: 'Mozilla Firefox', link: 'https://www.mozilla.org/ru/firefox/'},
+        {name: 'Chrome', link: 'https://www.google.com/chrome/'}
+        ]
+</script>
+
+<center>
+    <h2>К соажелнию наше приложожение работает только в браузерах Chrome и Mozilla Firefox</h2>
+    <ul>
+		{#each browsers as browser, index}
+			<li>
+				Скачать <a href={browser.link}>{browser.name}</a>
+			</li>
+		{/each}
+    </ul>
+</center>
+
+<style>
+    h2 {
+        width: 50%;
+    }
+    ul {
+        list-style: none;
+        margin: 0px;
+        padding: 0px;;
+    }
+
+    li {
+        text-align: center;
+        padding: 0.8em;
+        border-bottom: 1px solid lightgray;
+    }
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
+const code = navigator.appCodeName;
 import App from "./app.svelte";
+import Browser from "./browser.svelte";
+
+function browserCheck () {
+    if(code == 'Mozilla' || code == 'Chrome') {
+        const app = new App({
+            target: document.body,
+        });
+
+    }
+    else {
+        const browser = new Browser({
+            target:document.body
+        })
+    }
+}
+
+browserCheck();
 
 
-const app = new App({
-    target: document.body,
-});
+
+
