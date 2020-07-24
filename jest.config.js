b24c0f1a53653e9b2df876bf6fa6f28c32012e10
34x <maks@34x.me>
2020-07-24 22:12:34 +0200

    Put some of the test configs back

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   ...require('@snowpack/app-scripts-svelte/jest.config.js')(),
-  modulePaths: ['<rootDir>/src'],
-  moduleNameMapper: {
-	  '^src(.*)': '<rootDir>src$1',
-	  '^public(.*)': '<rootDir>public$1'
-  }
+  // modulePaths: ['<rootDir>/src'],
+  // moduleNameMapper: {
+	 //  '^src(.*)': '<rootDir>src$1',
+	 //  '^public(.*)': '<rootDir>public$1'
+  // }
 };
