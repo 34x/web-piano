8670dfb86120b1836bee7c78ce38b448bcd618c6
34x <maks@34x.me>
2020-07-25 09:21:35 +0200

    Use midi player js module  import check

--- src/browser.test.ts
+++ src/browser.test.ts
@@ -1,12 +1,13 @@
 import { render } from "@testing-library/svelte";
 import App from "src/browser.svelte";
 import { browsers } from 'src/components/browser';
+import MidiPlayerJS from 'midi-player-js';
 
 test("renders browsers page", () => {
 	expect(true).toBeTruthy();
 	expect(browsers).toBeTruthy();
 	expect(App).toBeTruthy();
-	expect(render).toBeTruthy();
+	expect(MidiPlayerJS).toBeTruthy();
   // const { getByText } = render(App);
   // const linkElement: HTMLElement = getByText(/Firefox/);
   // expect(linkElement).toBeInTheDocument();
