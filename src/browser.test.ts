9329547877a991568480e23789b0c44a82ac2cc6
34x <maks@34x.me>
2020-07-25 09:15:35 +0200

    Use svelte component import

--- src/browser.test.ts
+++ src/browser.test.ts
@@ -3,9 +3,9 @@ import App from "src/browser.svelte";
 import { browsers } from 'src/components/browser';
 
 test("renders browsers page", () => {
-	// expect(render).toBeTruthy();
 	expect(true).toBeTruthy();
 	expect(browsers).toBeTruthy();
+	expect(App).toBeTruthy();
   // const { getByText } = render(App);
   // const linkElement: HTMLElement = getByText(/Firefox/);
   // expect(linkElement).toBeInTheDocument();
