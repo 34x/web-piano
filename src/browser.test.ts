e8bbf585fcb6a4a6a4878d6049e049e5d626fca1
34x <maks@34x.me>
2020-07-25 09:40:26 +0200

    -

--- src/browser.test.ts
+++ src/browser.test.ts
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/svelte";
 import App from "src/browser.svelte";
 import { browsers } from 'src/components/browser';
-import MidiPlayerJS from 'midi-player-jsaaa';
+import MidiPlayerJS from 'midi-player-js';
 
 test("renders browsers page", () => {
 	expect(true).toBeTruthy();
