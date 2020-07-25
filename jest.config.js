cf23ca4f43a8680df844eb30156d52abca35afed
34x <maks@34x.me>
2020-07-25 08:11:44 +0200

    Leave only modules path in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   ...require('@snowpack/app-scripts-svelte/jest.config.js')(),
   modulePaths: [
-      '<rootDir>/src',
-      '<rootDir>/public'
-  ],
-  moduleNameMapper: {
-      '^src(.*)': '<rootDir>/src$1',
-      '^public(.*)': '<rootDir>/public$1'
-  }
+      '<rootDir>',
+  ]
 };
