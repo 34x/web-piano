ab3b6dff7cff805a723c1298c905963ab14258f7
34x <maks@34x.me>
2020-07-25 08:20:43 +0200

    Minimize browser test

--- src/browser.test.ts
+++ src/browser.test.ts
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/svelte";
-import App from "./browser.svelte";
+// import App from "./browser.svelte";
 
 test("renders browsers page", () => {
-  const { getByText } = render(App);
-  const linkElement: HTMLElement = getByText(/Firefox/);
-  expect(linkElement).toBeInTheDocument();
+  // const { getByText } = render(App);
+  // const linkElement: HTMLElement = getByText(/Firefox/);
+  // expect(linkElement).toBeInTheDocument();
 });
