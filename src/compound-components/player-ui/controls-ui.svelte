63ba214b196f219ce25da044bca26d151536ad1a
Artem <artnerab@gmail.com>
2020-08-10 18:32:34 +0300

    Make the button inactive while the song is loading

--- src/compound-components/player-ui/controls-ui.svelte
+++ src/compound-components/player-ui/controls-ui.svelte
@@ -11,13 +11,16 @@
     
     let playerState = player.getState();
     let progress = 0;
+    let loading = true;
 
     player.onStateChange(event => playerState = event.state);
     player.onProgressChange(event => progress = event.progress);
 
 
     async function loadSongUrl(filename) {
+        loading = true;
         await player.loadUrl('/midi/' + filename);
+        loading = false;
         midiFileInfo = player.getInfo();
     }
 
@@ -41,17 +44,34 @@
     const getButtonImage = (state) => state === MidiPlayerState.playing ? './pause.png' : './play.png';
 
     $: buttonImage = getButtonImage(playerState);
+    
 
 </script>
 
 <center>
-    <img on:click={playBtnHandler} src={buttonImage} alt="кнопка играть">
+    {#if loading}
+        <button disabled style="filter: opacity(0.5);">
+            <img  src={buttonImage} alt="кнопка играть" >
+        </button>
+    {:else}
+        <button on:click={playBtnHandler}>
+            <img  src={buttonImage} alt="кнопка играть">
+        </button>
+    {/if}
     <div id="greyProgress">
         <div style="width: {progress}%" id="greenBar"></div>
       </div>
 </center>
 
 <style>
+    button {
+        padding: 0;
+        border: none;
+        font: inherit;
+        color: inherit;
+        background-color: transparent;
+        outline: none;
+    }
     img {
         width: 128px;
         height: 128px;
